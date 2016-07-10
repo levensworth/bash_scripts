33850b3f4dc026cb92412973048c075a58617757
santiagobassani96 <santiago.bassani96@gmail.com>
2016-07-10 18:29:39 -0300

    mean

--- mean.sh
+++ mean.sh
@@ -11,15 +11,29 @@ do
     read number
     checks $number $escape
     want_exit=$?
-    require_number $number please_a_number 2
+    require_number $number ok 2
     if [ $? -eq 0 ]
     then
         numbers[$index]=$number
+        let index=$index+1
     elif [ $want_exit -eq 0 ]
     then
         finish=0
     else
+        echo
         echo " that wasn't a number so it wasn't count"
         echo # give a space for better understanding
     fi
 done
+mean(){
+    sum=0
+    for item in "${numbers[@]}"
+    do
+        let sum=$sum+$item
+
+    done
+    echo $sum
+    let mean_val=$sum/$index
+}
+mean
+echo " the result is $mean_val"
